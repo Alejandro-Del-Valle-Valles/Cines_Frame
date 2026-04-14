2fa766a50e19c4dfe93b91b2eb70a75db414c5b8
Alejandro Del Valle <alejandrodelvalle06@outlook.es>
2026-04-14 13:10:48 +0200

    Empiezo a implementar el repositorio de participantes

--- composeApp/src/commonMain/kotlin/com/alejandro/proyecto_cines_frame/data/repository/ParticipanteRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/alejandro/proyecto_cines_frame/data/repository/ParticipanteRepositoryImpl.kt
@@ -1,9 +1,54 @@
 package com.alejandro.proyecto_cines_frame.data.repository
 
+import com.alejandro.proyecto_cines_frame.core.error.ApiResult
+import com.alejandro.proyecto_cines_frame.data.adapter.ParticipanteAdpater
 import com.alejandro.proyecto_cines_frame.data.remote.api.interfaces.ParticipanteApi
+import com.alejandro.proyecto_cines_frame.data.remote.dto.ParticipanteDTO
+import com.alejandro.proyecto_cines_frame.data.remote.error.toAppError
+import com.alejandro.proyecto_cines_frame.domain.model.Participante
 import com.alejandro.proyecto_cines_frame.domain.repository.ParticipanteRepository
 
 class ParticipanteRepositoryImpl(
     private val api: ParticipanteApi
 ) : ParticipanteRepository {
+
+    /**
+     * Obtiene todos los participantes.
+     */
+    override suspend fun getAll(): ApiResult<List<Participante>> {
+        return try {
+            val dtos = api.getAll()
+            ApiResult.Success(dtos.map { ParticipanteAdpater.toParticipante(it)})
+        } catch (t: Throwable) {
+            ApiResult.Error(t.toAppError())
+        }
+    }
+
+    /**
+     * Obtiene todos los partiicpantes por su nombre
+     */
+    override suspend fun getAllByNombre(nombre: String): ApiResult<List<Participante>> {
+        return try {
+            val dtos = api.getAllByNombre(nombre)
+            ApiResult.Success(dtos.map { ParticipanteAdpater.toParticipante(it)})
+        } catch (t: Throwable) {
+            ApiResult.Error(t.toAppError())
+        }
+    }
+
+    override suspend fun getById(id: Int): ApiResult<ParticipanteDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createParticipante(nombre: String): ApiResult<ParticipanteDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateParticipante(participante: ParticipanteDTO): ApiResult<ParticipanteDTO> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteParticipante(id: Int): ApiResult<ParticipanteDTO> {
+        TODO("Not yet implemented")
+    }
 }
--- composeApp/src/commonMain/kotlin/com/alejandro/proyecto_cines_frame/domain/repository/ParticipanteRepository.kt
+++ composeApp/src/commonMain/kotlin/com/alejandro/proyecto_cines_frame/domain/repository/ParticipanteRepository.kt
@@ -1,4 +1,14 @@
 package com.alejandro.proyecto_cines_frame.domain.repository
 
+import com.alejandro.proyecto_cines_frame.core.error.ApiResult
+import com.alejandro.proyecto_cines_frame.data.remote.dto.ParticipanteDTO
+import com.alejandro.proyecto_cines_frame.domain.model.Participante
+
 interface ParticipanteRepository {
+    suspend fun getAll() : ApiResult<List<Participante>>
+    suspend fun getAllByNombre(nombre: String) : ApiResult<List<Participante>>
+    suspend fun getById(id: Int) : ApiResult<ParticipanteDTO>
+    suspend fun createParticipante(nombre: String) : ApiResult<ParticipanteDTO>
+    suspend fun updateParticipante(participante: ParticipanteDTO) : ApiResult<ParticipanteDTO>
+    suspend fun deleteParticipante(id: Int) : ApiResult<ParticipanteDTO>
 }
