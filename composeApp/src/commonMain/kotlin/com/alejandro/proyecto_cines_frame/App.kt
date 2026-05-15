e114ccc124ef010db37017992a95bb4880a4fea1
Alejandro <alejandrodelvalle06@outlook.es>
2026-05-15 14:34:45 +0200

    Elimino el lanzamiento de la AddEditScreen para películas del App.kt

--- composeApp/src/commonMain/kotlin/com/alejandro/proyecto_cines_frame/App.kt
+++ composeApp/src/commonMain/kotlin/com/alejandro/proyecto_cines_frame/App.kt
@@ -2,7 +2,6 @@ package com.alejandro.proyecto_cines_frame
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.alejandro.proyecto_cines_frame.ui.components.admin.MovieManagementScreens.AddEditMovie.AddEditMovieScreen
 import com.alejandro.proyecto_cines_frame.ui.screen.MainScreen
 import com.alejandro.proyecto_cines_frame.ui.theme.AppTheme
 
@@ -12,6 +11,5 @@ import com.alejandro.proyecto_cines_frame.ui.theme.AppTheme
 fun App() {
     AppTheme {
         MainScreen()
-        //AddEditMovieScreen(null, {})
     }
 }
